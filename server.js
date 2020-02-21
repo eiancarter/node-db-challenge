380fbe578bab46f4817281ca9995a87b9469c0c0
Eian Carter <eian@bricleir.com>
2020-02-21 10:46:01 -0800

    mvp plus stretch complete

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ const ResourceRouter = require("./api/resource-router")
 const server = express();
 
 server.use(express.json());
-server.use('api/projects', ProjectRouter);
-server.use('api/resources', ResourceRouter);
+server.use('/api/projects', ProjectRouter);
+server.use('/api/resources', ResourceRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h3>hello world</h3>');
+});
 
 module.exports = server;
